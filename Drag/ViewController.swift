c62ef1467e239095faaf50bf1470d390a86aa2dd
MarinaArai <maru1202disney@i.softbank.jp>
2015-09-07 21:55:08 +0900

    Initial Commit

---
+++ Drag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drag
+//
+//  Created by Arai Marina on 2015/09/07.
+//  Copyright (c) 2015年 Arai Marina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

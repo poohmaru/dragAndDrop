c042aa0aaaf2b743af1c8dc5dd75488b907e4fa7
MarinaArai <maru1202disney@i.softbank.jp>
2015-09-10 18:43:52 +0900

    test

--- Drag/ViewController.swift
+++ Drag/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var panGesture: UIPanGestureRecognizer!
+    @IBOutlet var panGestureblue: UIPanGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,5 +29,11 @@ class ViewController: UIViewController {
         sender.setTranslation(CGPointZero, inView: self.view)
     }
 
+    @IBAction func bluePan(sender: UIPanGestureRecognizer) {
+        var point: CGPoint = sender.translationInView(self.view)
+        var movedPoint: CGPoint = CGPointMake(sender.view!.center.x + point.x, sender.view!.center.y + point.y)
+        sender.view!.center = movedPoint
+        sender.setTranslation(CGPointZero, inView: self.view)
+    }
 }
 
